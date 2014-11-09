975623b577c60027928c353b7642b5edd5824a05
Amanda McDermott <amanda.p.mcdermott@gmail.com>
2014-11-08 22:17:57 -0500

    Implement sum and avg

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
-function make_li(n) {
-	return "<li>" + n + "</li>";
+function sum(values) {
+	return _.reduce(values, function(sum, n) { return sum + n; }, 0);
 }
+
+var count = 0;
+var values = [];
+var avgs = []
 
 $("#btn").on("click", function() {
 	var n = _.random(0, 100);
-	$("#list").prepend(make_li(n));
+	values.push(n);
+	count++;
+
+	$("#list").prepend("<li>" + n + "</li>");
+
+	var sum1 = parseInt($("#sum").text()) + n;
+	$("#sum").text(sum1);
+
+	var avg = sum1 / count;
+	$("#avg").text(avg);
+	avgs.push(avg);
+
+	var sum2 = sum(values);
+
+	console.log(
+		"Sum", sum2, 
+		"Length", values.length, 
+		"Avg", sum2 / values.length);
 });
 
 
